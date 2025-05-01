38add22bdc511617899599249fcbf1c907a7eee4
RafaelFernaa <josepatrickhotmail710@gmail.com>
2025-05-01 04:38:56 -0300

    constantes para definir dimensoes das matrizes, melhoria de exibição do resultado

--- Q6.c
+++ Q6.c
@@ -1,37 +1,56 @@
 #include <stdio.h>
 
+#define LINHAS 3
+#define COLUNAS 3
+
 int main() {
-    int matriz1[3][3];
-    int matriz2[3][3];
-    int resultado[3][3];
+    int matriz1[LINHAS][COLUNAS];
+    int matriz2[LINHAS][COLUNAS];
+    int resultado[LINHAS][COLUNAS];
     int i, j;
     
-    printf("Digite os valores da primeira matriz 3x3:\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
+    printf("Digite os valores da primeira matriz %dx%d:\n", LINHAS, COLUNAS);
+    for (i = 0; i < LINHAS; i++) {
+        for (j = 0; j < COLUNAS; j++) {
             printf("Matriz 1 [%d][%d]: ", i, j);
             scanf("%d", &matriz1[i][j]);
         }
     }
     
-    printf("\nDigite os valores da segunda matriz 3x3:\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
+    printf("\nDigite os valores da segunda matriz %dx%d:\n", LINHAS, COLUNAS);
+    for (i = 0; i < LINHAS; i++) {
+        for (j = 0; j < COLUNAS; j++) {
             printf("Matriz 2 [%d][%d]: ", i, j);
             scanf("%d", &matriz2[i][j]);
         }
     }
     
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
+    printf("\nPrimeira matriz:\n");
+    for (i = 0; i < LINHAS; i++) {
+        for (j = 0; j < COLUNAS; j++) {
+            printf("%d\t", matriz1[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("\nSegunda matriz:\n");
+    for (i = 0; i < LINHAS; i++) {
+        for (j = 0; j < COLUNAS; j++) {
+            printf("%d\t", matriz2[i][j]);
+        }
+        printf("\n");
+    }
+    
+    for (i = 0; i < LINHAS; i++) {
+        for (j = 0; j < COLUNAS; j++) {
             resultado[i][j] = matriz1[i][j] + matriz2[i][j];
         }
     }
     
     printf("\nResultado da soma das matrizes:\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            printf("%d ", resultado[i][j]);
+    for (i = 0; i < LINHAS; i++) {
+        for (j = 0; j < COLUNAS; j++) {
+            printf("%d\t", resultado[i][j]);
         }
         printf("\n");
     }
