e8bd0ba1f3dbd0a073c2e66677bec9abd3ca7411
RafaelFernaa <josepatrickhotmail710@gmail.com>
2025-05-01 04:20:54 -0300

    adição de array que armazena frequencias e melhorias na logica

--- Q4.c
+++ Q4.c
@@ -2,32 +2,35 @@
 
 int main() {
     int numeros[10];
+    int frequencia[10];
     int i, j;
-    int contagem;
-    int verificado[10] = {0};
+    int contador = 0;
     
     for (i = 0; i < 10; i++) {
         printf("Digite o %do numero: ", i+1);
         scanf("%d", &numeros[i]);
+        frequencia[i] = -1;
     }
     
-    printf("\nFrequencia dos numeros:\n");
-    
     for (i = 0; i < 10; i++) {
-        if (verificado[i] == 1) {
-            continue;
-        }
-        
-        contagem = 1;
-        
+        contador = 1;
         for (j = i + 1; j < 10; j++) {
             if (numeros[i] == numeros[j]) {
-                contagem++;
-                verificado[j] = 1;
+                contador++;
+                frequencia[j] = 0;
             }
         }
         
-        printf("O numero %d aparece %d vez(es)\n", numeros[i], contagem);
+        if (frequencia[i] != 0) {
+            frequencia[i] = contador;
+        }
+    }
+    
+    printf("\nFrequencia dos numeros:\n");
+    for (i = 0; i < 10; i++) {
+        if (frequencia[i] != 0) {
+            printf("O numero %d aparece %d vez(es)\n", numeros[i], frequencia[i]);
+        }
     }
     
     return 0;
