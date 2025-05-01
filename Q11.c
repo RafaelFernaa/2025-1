d7f8433d70725c62110735313413cf530d3dafb4
RafaelFernaa <josepatrickhotmail710@gmail.com>
2025-05-01 19:45:20 -0300

    implementação de função para ordenar o vetor

--- Q11.c
+++ Q11.c
@@ -20,3 +20,15 @@ int buscaBinaria(int vetor[], int tamanho, int chave) {
     
     return -1;
 }
+
+void ordenarVetor(int vetor[], int tamanho) {
+    for (int i = 0; i < tamanho - 1; i++) {
+        for (int j = 0; j < tamanho - i - 1; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                int temp = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = temp;
+            }
+        }
+    }
+}
