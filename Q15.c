bb834b47b7a6dce04aaae3bc62420bae97533a9b
RafaelFernaa <josepatrickhotmail710@gmail.com>
2025-05-01 21:13:57 -0300

    Implementação das funções auxiliares para interface

--- Q15.c
+++ Q15.c
@@ -21,3 +21,43 @@ char *palavras[] = {
 };
 
 int total_palavras = 10;
+
+void limparTela() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
+
+void desenharForca(int erros) {
+    printf("\n  _______\n");
+    printf("  |     |\n");
+    
+    if (erros >= 1) {
+        printf("  |     O\n");
+    } else {
+        printf("  |\n");
+    }
+    
+    if (erros == 2) {
+        printf("  |     |\n");
+    } else if (erros == 3) {
+        printf("  |    /|\n");
+    } else if (erros >= 4) {
+        printf("  |    /|\\\n");
+    } else {
+        printf("  |\n");
+    }
+    
+    if (erros == 5) {
+        printf("  |    /\n");
+    } else if (erros >= 6) {
+        printf("  |    / \\\n");
+    } else {
+        printf("  |\n");
+    }
+    
+    printf("  |\n");
+    printf("__|__\n");
+}
